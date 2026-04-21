f32e0007f0d3c6cea7140ea58621e66e7bd0ef29
Joseph Joshua <calebayuba222@gmail.com>
2026-04-22 00:23:24 +0100

    refactor: clean up code base

---
+++ FIXES.md
@@ -0,0 +1,4 @@
+worker.py, line 3 and 4, unused import of os and signal module, removed the unused import
+main.py, line 4, unused os module, removed unused import
+.gitignore, add gitignore file for env and dev files
+README>md, the file wasn't detailed, made it well detailed and explain how the project works
--- api/main.py
+++ api/main.py
@@ -1,11 +1,11 @@
 from fastapi import FastAPI
 import redis
 import uuid
-import os
 
 app = FastAPI()
 
 r = redis.Redis(host="localhost", port=6379)
+
 
 @app.post("/jobs")
 def create_job():
@@ -13,6 +13,7 @@ def create_job():
     r.lpush("job", job_id)
     r.hset(f"job:{job_id}", "status", "queued")
     return {"job_id": job_id}
+
 
 @app.get("/jobs/{job_id}")
 def get_job(job_id: str):
